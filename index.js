f8e321f7503a4bde6629ca947a6de794c45ba91e
Ting Chung <tchung682@gmail.com>
2020-05-11 14:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function add(one,two){
   return one + two;
 }
+function subtract(a,b){
+  return a - b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function increment(n){
+  return n++;
+}
+function decrement(n){
+  return n--;
+}
