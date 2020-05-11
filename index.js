49c7847bbe569a3a197aef7559d4bd3bdb47927c
Ting Chung <tchung682@gmail.com>
2020-05-11 14:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function divide(a,b){
   return a/b;
 }
 function increment(n){
-  return n++;
+  return n+=1;
 }
 function decrement(n){
-  return n--;
+  return n-=1;
 }
 function makeInt(n){
-  parseInt(n,10);
+  return parseInt(n,10);
 }
 function preserveDecimal(n){
-  parseFloat(n);
+  return parseFloat(n);
 }
